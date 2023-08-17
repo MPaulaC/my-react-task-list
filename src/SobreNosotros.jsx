47bd63f715fff5c480b22308ad441431b3769dbf
Paula <mpcastanedagarcia@gmail.com>
2023-08-17 15:01:17 -0500

    agregue rutas

---
+++ src/SobreNosotros.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SobreNosotros = () => {
+  return (
+    <div>
+      <h1>SobreNosotros</h1>
+      <p>
+                Esta es una aplicación de tareas desarrollada con React. Permite al
+                usuario agregar, eliminar y marcar tareas como completadas.
+            </p>
+            <p>Tecnologías utilizadas: React, React Router.</p>
+    </div>
+  );
+};
+
+export default SobreNosotros;
--- src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-function Header() {
-    return (<h2>Task app</h2>)
-}
-
-export default Header;
